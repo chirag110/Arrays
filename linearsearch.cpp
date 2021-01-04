1971e89b779d15797b94d1f82a007e3418d288d0
chirag110 <chiragguglani@yahoo.in>
2021-01-04 19:43:37 +0530

    linearsearch

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 int main(){
     int n,key;
-    cout<<"Enter Array size:";
+    //cout<<"Enter Array size:";
     cin>>n;
-    cout<<"Enter Key to be searched:";
+    //cout<<"Enter Key to be searched:";
     cin>>key;
     int a[n];
-    cout<<"Enter the array elements:";
+    //cout<<"Enter the array elements:";
     int i;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
     for(i=0;i<n-1;i++){
         if(a[i]==key){
-            cout<<key<<"found at index"<<i;
+            cout<<key<<"  found at index  "<<i<<endl;
             break;
         }
     }
